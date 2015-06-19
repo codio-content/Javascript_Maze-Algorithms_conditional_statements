57a03d6bad0c72238952d02fe4255f899a8ff9e4
SeanPackham <sean@seanpackham.com>
2015-06-19 12:46:51 +0000

    tests complete

--- .guides/tests/ch-7.js
+++ .guides/tests/ch-7.js
@@ -1,8 +1,39 @@
 
+keyPressedEvent  = null;
+
 $.getScript(window.location.origin + '/public/js/' + window.testEnv.cmd + '.js?_=' + Date.now())
 .done(function (script, status) {
-  // console.log(_commands);
-  codio.setButtonValue(window.testEnv.id, codio.BUTTON_STATE.INVALID, "Test not implemented yet. Coming soon.");  
+  
+  var _if = false;
+  var _else = false;
+  
+  if(typeof turnTaken == 'function') {
+  
+    window.showMessage = function(val) {
+      if(val.toLowerCase() == 'low energy') _if = true;
+    }
+    
+    energy = 9;
+    turnTaken();
+    
+    window.showMessage = function(val) {
+      if(val.toLowerCase() == 'energy ok') _else = true;
+    }
+    
+    energy = 10;
+    turnTaken();
+    
+
+    if(!_if || !_else ) {
+      return codio.setButtonValue(window.testEnv.id, codio.BUTTON_STATE.FAILURE, 'Not quite right, try again!');
+    }   
+  }
+  else {
+    return codio.setButtonValue(window.testEnv.id, codio.BUTTON_STATE.FAILURE, 'Not quite right, try again!');
+  }
+  
+  codio.setButtonValue(window.testEnv.id, codio.BUTTON_STATE.SUCCESS, 'Well done!');    
+
 })
 .fail(function (jqxhr, settings, exception) {
   console.log(exception);
