6d6d4da537d368d43e2a172b1358435609b736eb
Ian Jobling <ijobling@codio.com>
2016-06-29 14:11:33 +0100

    Update ch-1.js
    remove completed code

--- public/js/ch-1.js
+++ public/js/ch-1.js
@@ -4,15 +4,6 @@ function keyPressedEvent(keyCode) {
   if ( keyCode == 'LEFT' ) {
     player.moveLeft()
   } 
-	if ( keyCode == 'RIGHT' ) {
-    player.moveRight()
-  } 
-  if ( keyCode == 'UP' ) {
-    player.moveUp()
-  } 
-  if ( keyCode == 'DOWN' ) {
-    player.moveDown()
-  }
- 
+
 }
 
